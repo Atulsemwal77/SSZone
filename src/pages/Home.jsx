581a13970afc6f6d1808ab4fee04d9bdd7043a16
bhupendra gariya <bhupiigarya7@gmail.com>
2025-04-26 16:52:12 +0530

    fix the home paget

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { ArrowRight, Plus, X } from "lucide-react";
+import { FaStar } from "react-icons/fa6";
+import { FaRegStarHalfStroke } from "react-icons/fa6";
 import hero from "../assets/image/hero.png";
 import blog1 from "../assets/image/blog1.jpg";
 import blog2 from "../assets/image/blog2.jpg";
 import blog3 from "../assets/image/blog3.jpg";
 import card from "../assets/image/card.jpg";
+import online from "../assets/image/student.jpg";
+import vodafone from "../assets/image/vodafone.png";
+import intel from "../assets/image/intel.png";
+import amd from "../assets/image/amd.png";
+import talkit from "../assets/image/talkit.png";
+import tesla from "../assets/image/tesla.png";
+import Infosys from "../assets/image/Infosys.png";
+import image1 from "../assets/image/image04.png";
+import image2 from "../assets/image/pexels-julia-m-cameron-4144224 1.png";
 
 import CourseCard from "../componant/CourseCard";
-import company from "../assets/image/company.png";
+// import company from "../assets/image/company.png";
 import BlogCard from "../componant/BlogCard";
 
 const courses = [
@@ -120,9 +131,6 @@ const blog = [
   },
 ];
 
-
-
-
 const faqs = [
   {
     question: "What courses do you offer?",
@@ -156,12 +164,7 @@ const faqs = [
   },
 ];
 
-
-
-
-
 const categories = [
-  
   "All Categories",
   "Trending",
   "Design",
@@ -171,8 +174,6 @@ const categories = [
 ];
 
 function Home() {
-
-
   const [active, setActive] = useState("All Categories");
   const [activeIndex, setActiveIndex] = useState(null);
 
@@ -184,12 +185,11 @@ function Home() {
     }
   };
 
-
-
   const filteredCourses =
     active === "All Categories"
       ? courses
       : courses.filter((course) => course.category === active);
+
   return (
     <div>
       <section className="px-4 md:px-16 mx-auto   ">
@@ -213,32 +213,48 @@ function Home() {
 
           <div className="relative mt-20 lg:mt-0 lg:w-1/2 flex justify-center">
             <div className="relative z-10">
-              <img src={hero} alt="Student" className="  lg:rounded-lg md:w-full w-80" />
+              <img
+                src={hero}
+                alt="Student"
+                className="  lg:rounded-lg md:w-full w-80"
+              />
 
               <div className="absolute md:top-40 top-0 md:left-4 -left-6 bg-white rounded-lg shadow px-4 py-2 flex items-center ">
-                <span className="text-blue-600 font-bold text-sm">50+</span>
-                <span className="text-gray-600 text-sm">Online Courses</span>
+                <div className="flex items-center gap-1">
+                  <div>
+                    <img src="" alt="" />
+                  </div>
+                  <div className=" ">
+                    <p className="text-blue-600 font-bold text-sm justify-self-center">
+                      50+
+                    </p>
+                    <p className="text-gray-600 text-sm justify-self-center">
+                      Online Courses
+                    </p>
+                  </div>
+                </div>
               </div>
 
               <div className="absolute  lg:-bottom-5 -bottom-15 lg:left-65  md:left-50  left-20 bg-white rounded-lg shadow px-4 py-2">
-                <p className="text-sm font-semibold text-gray-700">
-                  10K+ Online Students
+                <p className="justify-self-center">10K+</p>
+                <p className="text-sm font-semibold text-gray-700 justify-self-center">
+                  Online Students
                 </p>
                 <div className="flex  mt-1">
                   <img
-                    src="/avatars/user1.jpg"
+                    src={online}
                     className="w-6 h-6 rounded-full border-2 border-white"
                   />
                   <img
-                    src="/avatars/user2.jpg"
+                    src={online}
                     className="w-6 h-6 rounded-full border-2 border-white"
                   />
                   <img
-                    src="/avatars/user3.jpg"
+                    src={online}
                     className="w-6 h-6 rounded-full border-2 border-white"
                   />
                   <img
-                    src="/avatars/user4.jpg"
+                    src={online}
                     className="w-6 h-6 rounded-full border-2 border-white"
                   />
                 </div>
@@ -251,12 +267,44 @@ function Home() {
           </div>
         </div>
 
-        <img src={company} alt="" className="lg:py-10 pt-30 lg:pt-0" />
+        <div className=" relative w-full overflow-hidden px-3">
+          <div className="flex w-full justify-between gap-8 my-12 scrolling">
+            <img src={vodafone} alt="vodafone" />
+            <img src={intel} alt="intel" />
+            <img src={tesla} alt="tesla" />
+            <img src={amd} alt="amd" />
+            <img src={talkit} alt="talkit" />
+            <img src={Infosys} alt="Infosys" />
+
+            {/* Duplicate for Smooth Infinite Scrolling */}
+
+            <img src={vodafone} alt="vodafone" />
+            <img src={intel} alt="intel" />
+            <img src={tesla} alt="tesla" />
+            <img src={amd} alt="amd" />
+            <img src={talkit} alt="talkit" />
+            <img src={Infosys} alt="Infosys" />
+
+            <img src={vodafone} alt="vodafone" />
+            <img src={intel} alt="intel" />
+            <img src={tesla} alt="tesla" />
+            <img src={amd} alt="amd" />
+            <img src={talkit} alt="talkit" />
+            <img src={Infosys} alt="Infosys" />
+
+            <img src={vodafone} alt="vodafone" />
+            <img src={intel} alt="intel" />
+            <img src={tesla} alt="tesla" />
+            <img src={amd} alt="amd" />
+            <img src={talkit} alt="talkit" />
+            <img src={Infosys} alt="Infosys" />
+          </div>
+        </div>
 
         <div className="">
-          <div className="py-10 justify-self-center items-center">
+          <div className="py-5 justify-self-center items-center">
             <div>
-              <p className="text-blue-400 justify-self-center   py-3">
+              <p className="text-blue-700 font-bold justify-self-center   py-3">
                 POPULAR COURSES
               </p>
             </div>
@@ -271,23 +319,23 @@ function Home() {
                 education platform pursue a new career.
               </p>
             </div>
-         
-          <div className="flex gap-4 flex-wrap py-3">
-            {categories.map((category) => (
-              <button
-                key={category}
-                onClick={() => setActive(category)}
-                className={`px-4 py-2 rounded-full border text-sm font-medium transition duration-300
+
+            <div className="flex gap-4 flex-wrap py-3">
+              {categories.map((category) => (
+                <button
+                  key={category}
+                  onClick={() => setActive(category)}
+                  className={`px-4 py-2 rounded-full border text-sm font-medium transition duration-300 cursor-pointer
               ${
                 active === category
                   ? "bg-blue-600 text-white"
                   : "bg-white text-blue-600 border-blue-500 hover:bg-blue-100"
               }`}
-              >
-                {category}
-              </button>
-            ))}
-          </div>
+                >
+                  {category}
+                </button>
+              ))}
+            </div>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-self-center ">
@@ -304,18 +352,49 @@ function Home() {
               />
             ))}
           </div>
-          <button className="bg-blue-600 hover:bg-blue-700 mt-10 text-white px-6 py-3 rounded-lg flex items-center justify-self-center gap-2 transition  cursor-pointer duration-300 ease-in-out  hover:scale-105">
+          <button className="bg-blue-600 hover:bg-blue-700 mt-5 text-white px-6 py-3 rounded-lg flex items-center justify-self-center gap-2 transition  cursor-pointer duration-300 ease-in-out  hover:scale-105 ">
             Explore All Courses <ArrowRight size={20} />
           </button>
         </div>
 
-        {/* only Start Learning With Skills
-Hunt Now. */}
+        <div className=" flex flex-col lg:flex-row justify-around items-center gap-20 pt-10 p-2 ">
+          <div className=" max-w-[500px] justify-self-start ">
+            <h1 className="text-4xl my-8">
+              Start Learning With Skills Hunt Now.
+            </h1>
+            <p className="text-sm">
+              Tap into your full potential with online courses guided by
+              industry experts. Master high-demand skills on your schedule and
+              take control of your career journey today.
+            </p>
+            <div className="grid grid-cols-2 gap-2 my-8">
+              <li className=" list-none">Start learning from our experts.</li>
+              <li className=" list-none">Enhance your skills with us now.</li>
+              <li className=" list-none">Do the professional level Courses</li>
+              <li className=" list-none">Get our verified Certificate</li>
+            </div>
+            <button className=" flex gap-2 items-center bg-blue-700 text-white px-8 py-4 rounded-lg my-4  cursor-pointer duration-300 ease-in-out  hover:scale-105 ">
+              Browse Courses <ArrowRight className="text-xl " />
+            </button>
+          </div>
+          <div className=" relative">
+            <img
+              src={image1}
+              alt="image1"
+              className="h-[35vh] lg:h-[40vh] relative z-10 "
+            />
+            <img
+              src={image2}
+              alt="image2"
+              className="h-[40vh] absolute top-[-100px] right-[-60px] lg:right-[-100px]"
+            />
+          </div>
+        </div>
 
         <div>
-          <div className="py-10 justify-self-center items-center">
+          <div className="pt-10 justify-self-center items-center">
             <div>
-              <p className="text-blue-400 justify-self-center   py-3">
+              <p className="text-blue-700 font-bold justify-self-center   py-3">
                 OUR BLOG
               </p>
             </div>
@@ -346,10 +425,108 @@ Hunt Now. */}
           </div>
         </div>
 
+
+
+        <div className=" ">
+        <div className="pt-10 justify-self-center items-center">
+            <div>
+              <p className="text-blue-700 font-bold justify-self-center    py-3">
+              Testimonials
+              </p>
+            </div>
+            <div>
+              <h1 className="text-3xl font-bold justify-self-center  py-3">
+              What Our Student Says About Us
+              </h1>
+            </div>
+            <div>
+              <p className="justify-self-center  pt-3">
+              Hear real stories and experiences from students who’ve transformed their careers with our courses
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col lg:flex-row justify-center items-center gap-6 py-10">
+            <div className="flex flex-col justify-center items-center p-5 gap-4 shadow-xl bg-[#EBF5FF] max-w-[260px]">
+              <p className="text-center">
+                <span className="text-blue-500 text-3xl">“</span>
+                Studying online saved me a lot of money, and I didn't have to
+                commute. The quality of the courses was fantastic, and I could
+                learn at my own pace.{" "}
+              </p>
+              <div className="flex justify-center items-center gap-2">
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaRegStarHalfStroke className="text-xl text-amber-300" />
+              </div>
+              <img src="" alt="" className="size-13 rounded-full" />
+              <h2 className="text-xl font-bold">Sakshi Chaudhary</h2>
+              <h3 className="text-blue-600">UI/UX Designer</h3>
+            </div>
+
+            <div className="flex flex-col justify-center items-center p-5 gap-4 shadow-xl bg-[#EBF5FF] max-w-[260px]">
+              <p className="text-center">
+                <span className="text-blue-500 text-3xl">“</span>
+                Studying online saved me a lot of money, and I didn't have to
+                commute. The quality of the courses was fantastic, and I could
+                learn at my own pace.{" "}
+              </p>
+              <div className="flex justify-center items-center gap-2">
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaRegStarHalfStroke className="text-xl text-amber-300" />
+              </div>
+              <img src="" alt="" className="size-13 rounded-full" />
+              <h2 className="text-xl font-bold">Sakshi Chaudhary</h2>
+              <h3 className="text-blue-600">UI/UX Designer</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center p-5 gap-4 shadow-xl bg-[#EBF5FF] max-w-[260px]">
+              <p className="text-center">
+                <span className="text-blue-500 text-3xl">“</span>
+                Studying online saved me a lot of money, and I didn't have to
+                commute. The quality of the courses was fantastic, and I could
+                learn at my own pace.{" "}
+              </p>
+              <div className="flex justify-center items-center gap-2">
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaRegStarHalfStroke className="text-xl text-amber-300" />
+              </div>
+              <img src="" alt="" className="size-13 rounded-full" />
+              <h2 className="text-xl font-bold">Sakshi Chaudhary</h2>
+              <h3 className="text-blue-600">UI/UX Designer</h3>
+            </div>
+
+            <div className="flex flex-col justify-center items-center p-5 gap-4 shadow-xl bg-[#EBF5FF] max-w-[260px]">
+              <p className="text-center">
+                <span className="text-blue-500 text-3xl">“</span>
+                Studying online saved me a lot of money, and I didn't have to
+                commute. The quality of the courses was fantastic, and I could
+                learn at my own pace.{" "}
+              </p>
+              <div className="flex justify-center items-center gap-2">
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaStar className="text-xl text-amber-300" />
+                <FaRegStarHalfStroke className="text-xl text-amber-300" />
+              </div>
+              <img src="" alt="" className="size-13 rounded-full" />
+              <h2 className="text-xl font-bold">Sakshi Chaudhary</h2>
+              <h3 className="text-blue-600">UI/UX Designer</h3>
+            </div>
+          </div>
+        </div>
+
         <div>
           <div className="py-10 justify-self-center items-center">
             <div>
-              <p className="text-blue-400 justify-self-center   py-3">
+              <p className="text-blue-700 font-bold justify-self-center    py-3">
                 Quick Answers
               </p>
             </div>
